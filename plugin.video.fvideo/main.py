4c54320fd5f44d88109410a200a2349428029a28
Tai khoan online <taikhoanonlinevn@gmail.com>
2021-06-08 17:34:49 +0700

    Update main.py

--- plugin.video.fvideo/main.py
+++ plugin.video.fvideo/main.py
@@ -4,10 +4,17 @@
 """
 Example video plugin that is compatible with Kodi 19.x "Matrix" and above
 """
+# import CommonFunctions as common
+import logging
 import sys
 from urllib.parse import urlencode, parse_qsl
 import xbmcgui
 import xbmcplugin
+
+if hasattr(sys.modules["__main__"], "xbmc"):
+    xbmc = sys.modules["__main__"].xbmc
+else:
+    import xbmc
 
 # Get the plugin url in plugin:// notation.
 _URL = sys.argv[0]
@@ -16,15 +23,70 @@ _HANDLE = int(sys.argv[1])
 
 # Danh sach phim
 VIDEOS = {'Hanh dong': [{'name': 'Phim 1',
-                       'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/04/crab-screenshot.jpg',
-                       'video': 'https://download028.fshare.vn/dl/y-7TGXkhiJM945obCcRt3i5jU8Rgl0PHNoEEyoMBVV-WSeXCMHcZuSYQZeamWzmbK5k8HuotwqPJWvAB/%28Hard%20Sub%20Vi%E1%BB%87t%29%20-%20LONDON%20HAS%20FALLEN.mp4',
-                       'genre': 'Animals'},
-                      {'name': 'Phim 2',
-                       'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/04/alligator-screenshot.jpg',
-                       'video': 'https://download028.fshare.vn/dl/y-7TGXkhiJM945obCcRt3i5jU8Rgl0PHNoEEyoMBVV-WSeXCMHcZuSYQZeamWzmbK5k8HuotwqPJWvAB/%28Hard%20Sub%20Vi%E1%BB%87t%29%20-%20LONDON%20HAS%20FALLEN.mp4',
-                       'genre': 'Animals'}
-                      ]}
-					  
+                         'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/04/crab-screenshot.jpg',
+                         'video': 'https://download028.fshare.vn/dl/y-7TGXkhiJM945obCcRt3i5jU8Rgl0PHNoEEyoMBVV-WSeXCMHcZuSYQZeamWzmbK5k8HuotwqPJWvAB/%28Hard%20Sub%20Vi%E1%BB%87t%29%20-%20LONDON%20HAS%20FALLEN.mp4',
+                         'genre': 'Animals'},
+                        {'name': 'Phim 2',
+                         'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/04/alligator-screenshot.jpg',
+                         'video': 'https://download028.fshare.vn/dl/y-7TGXkhiJM945obCcRt3i5jU8Rgl0PHNoEEyoMBVV-WSeXCMHcZuSYQZeamWzmbK5k8HuotwqPJWvAB/%28Hard%20Sub%20Vi%E1%BB%87t%29%20-%20LONDON%20HAS%20FALLEN.mp4',
+                         'genre': 'Animals'}
+                        ]}
+
+
+# This function raises a keyboard for user input
+def getUserInput(title=u"Input", default=u"", hidden=False):
+    # log("", 5)
+    result = None
+
+    # Fix for when this functions is called with default=None
+    if not default:
+        default = u""
+
+    keyboard = xbmc.Keyboard(default, title)
+    keyboard.setHiddenInput(hidden)
+    keyboard.doModal()
+
+    if keyboard.isConfirmed():
+        result = keyboard.getText()
+
+    # log(repr(result), 5)
+    return result
+
+
+def addDir(name, url):
+    # Create a list item with a text label and a thumbnail image.
+    list_item = xbmcgui.ListItem(label=name)
+    list_item.setArt({'thumb': '',
+                      'icon': '',
+                      'fanart': ''})
+    list_item.setInfo('video', {'title': name,
+                                'genre': '',
+                                'mediatype': 'video'})
+    is_folder = True
+    # Add our item to the Kodi virtual folder listing.
+    xbmcplugin.addDirectoryItem(_HANDLE, url, list_item, is_folder)
+
+
+def addLink(name, url):
+    # Create a list item with a text label and a thumbnail image.
+    list_item = xbmcgui.ListItem(label=name)
+    # Set additional info for the list item.
+    # 'mediatype' is needed for skin to display info for this ListItem correctly.
+    list_item.setInfo('video', {'title': name,
+                                'genre': '',
+                                'mediatype': 'video'})
+    # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
+    # Here we use the same image for all items for simplicity's sake.
+    # In a real-life plugin you need to set each image accordingly.
+    list_item.setArt({'thumb': '', 'icon': '', 'fanart': ''})
+    # Set 'IsPlayable' property to 'true'.
+    # This is mandatory for playable items!
+    list_item.setProperty('IsPlayable', 'true')
+    is_folder = False
+    # Add our item to the Kodi virtual folder listing.
+    xbmcplugin.addDirectoryItem(_HANDLE, url, list_item, is_folder)
+
+
 def get_url(**kwargs):
     """
     Create a URL for calling the plugin recursively from the given set of keyword arguments.
@@ -72,92 +134,23 @@ def get_videos(category):
 
 
 def list_categories():
-    """
-    Create the list of video categories in the Kodi interface.
-    """
-    # Set plugin category. It is displayed in some skins as the name
-    # of the current section.
-    xbmcplugin.setPluginCategory(_HANDLE, 'My Video Collection')
-    # Set plugin content. It allows Kodi to select appropriate views
-    # for this type of content.
-    xbmcplugin.setContent(_HANDLE, 'videos')
     # Get video categories
     categories = get_categories()
     # Iterate through categories
     for category in categories:
-        # Create a list item with a text label and a thumbnail image.
-        list_item = xbmcgui.ListItem(label=category)
-        # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
-        # Here we use the same image for all items for simplicity's sake.
-        # In a real-life plugin you need to set each image accordingly.
-        list_item.setArt({'thumb': VIDEOS[category][0]['thumb'],
-                          'icon': VIDEOS[category][0]['thumb'],
-                          'fanart': VIDEOS[category][0]['thumb']})
-        # Set additional info for the list item.
-        # Here we use a category name for both properties for for simplicity's sake.
-        # setInfo allows to set various information for an item.
-        # For available properties see the following link:
-        # https://codedocs.xyz/xbmc/xbmc/group__python__xbmcgui__listitem.html#ga0b71166869bda87ad744942888fb5f14
-        # 'mediatype' is needed for a skin to display info for this ListItem correctly.
-        list_item.setInfo('video', {'title': category,
-                                    'genre': category,
-                                    'mediatype': 'video'})
-        # Create a URL for a plugin recursive call.
         # Example: plugin://plugin.video.example/?action=listing&category=Animals
         url = get_url(action='listing', category=category)
-        # is_folder = True means that this item opens a sub-list of lower level items.
-        is_folder = True
-        # Add our item to the Kodi virtual folder listing.
-        xbmcplugin.addDirectoryItem(_HANDLE, url, list_item, is_folder)
-    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
-    xbmcplugin.addSortMethod(_HANDLE, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
-    # Finish creating a virtual folder.
-    xbmcplugin.endOfDirectory(_HANDLE)
+        addDir(category, url)
 
 
 def list_videos(category):
-    """
-    Create the list of playable videos in the Kodi interface.
-
-    :param category: Category name
-    :type category: str
-    """
-    # Set plugin category. It is displayed in some skins as the name
-    # of the current section.
-    xbmcplugin.setPluginCategory(_HANDLE, category)
-    # Set plugin content. It allows Kodi to select appropriate views
-    # for this type of content.
-    xbmcplugin.setContent(_HANDLE, 'videos')
-    # Get the list of videos in the category.
     videos = get_videos(category)
     # Iterate through videos.
     for video in videos:
-        # Create a list item with a text label and a thumbnail image.
-        list_item = xbmcgui.ListItem(label=video['name'])
-        # Set additional info for the list item.
-        # 'mediatype' is needed for skin to display info for this ListItem correctly.
-        list_item.setInfo('video', {'title': video['name'],
-                                    'genre': video['genre'],
-                                    'mediatype': 'video'})
-        # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
-        # Here we use the same image for all items for simplicity's sake.
-        # In a real-life plugin you need to set each image accordingly.
-        list_item.setArt({'thumb': video['thumb'], 'icon': video['thumb'], 'fanart': video['thumb']})
-        # Set 'IsPlayable' property to 'true'.
-        # This is mandatory for playable items!
-        list_item.setProperty('IsPlayable', 'true')
         # Create a URL for a plugin recursive call.
         # Example: plugin://plugin.video.example/?action=play&video=http://www.vidsplay.com/wp-content/uploads/2017/04/crab.mp4
         url = get_url(action='play', video=video['video'])
-        # Add the list item to a virtual Kodi folder.
-        # is_folder = False means that this item won't open any sub-list.
-        is_folder = False
-        # Add our item to the Kodi virtual folder listing.
-        xbmcplugin.addDirectoryItem(_HANDLE, url, list_item, is_folder)
-    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
-    xbmcplugin.addSortMethod(_HANDLE, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
-    # Finish creating a virtual folder.
-    xbmcplugin.endOfDirectory(_HANDLE)
+        addLink(video['name'], url)
 
 
 def play_video(path):
@@ -192,18 +185,32 @@ def router(paramstring):
         elif params['action'] == 'play':
             # Play a video from a provided URL.
             play_video(params['video'])
+        elif params['action'] == 'phimid':
+            filmId()
         else:
             # If the provided paramstring does not contain a supported action
             # we raise an exception. This helps to catch coding errors,
             # e.g. typos in action names.
             raise ValueError('Invalid paramstring: {}!'.format(paramstring))
     else:
-        # If the plugin is called from Kodi UI without any parameters,
-        # display the list of video categories
+        url = get_url(action='phimid', category='theoid')
+        addDir('Nhap vao ID phim', url)
         list_categories()
+
+
+def filmId():
+    sinput = getUserInput('Film ID', '')
+    href = 'https://www.fshare.vn/file/' + sinput
+    url = get_url(action='play', video=href)
+    addDir(href, url)
+    addLink(href, url)
 
 
 if __name__ == '__main__':
     # Call the router function and pass the plugin call parameters to it.
     # We use string slicing to trim the leading '?' from the plugin call paramstring
     router(sys.argv[2][1:])
+
+xbmcplugin.addSortMethod(_HANDLE, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+# Finish creating a virtual folder.
+xbmcplugin.endOfDirectory(_HANDLE)
